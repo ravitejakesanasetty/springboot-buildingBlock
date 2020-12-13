6c87583ead5b224c9e0ec8af5299e9115a41a3db
ravitejakesanasetty <ravitejakesanasetty@gmail.com>
2020-12-13 20:33:12 +0530

    first commit

---
+++ src/main/java/com/stackSimplity/restServices/SpringbootBuildingBlockApplication.java
@@ -0,0 +1,13 @@
+package com.stackSimplity.restServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingBlockApplication.class, args);
+	}
+
+}
